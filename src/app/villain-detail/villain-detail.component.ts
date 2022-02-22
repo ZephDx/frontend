54a5befb247af777a46147fa7502c1b2b63e2cb6
Matheus Henrique Pereira dos Santos <matheusdxseti45@gmail.com>
2022-02-22 08:56:19 -0300

    a

--- src/app/villain-detail/villain-detail.component.ts
+++ src/app/villain-detail/villain-detail.component.ts
@@ -1,41 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Villain } from '../villain';
-
+import { VillainService } from '../villain.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
-import { VillainService } from '../villain.service';
+import { Villain } from '../villain';
 
 @Component({
   selector: 'app-villain-detail',
   templateUrl: './villain-detail.component.html',
-  styleUrls: ['./villain-detail.component.css']
+  styleUrls: ['./villain-detail.component.css'],
 })
 export class VillainDetailComponent implements OnInit {
-
- villain: Villain | undefined
+  villain: Villain | undefined;
 
   constructor(
-    private router:ActivatedRoute,
-    private villainService:VillainService,
+    private router: ActivatedRoute,
+    private villainService: VillainService,
     private location: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
+    this.getVillain();
   }
 
   getVillain(): void {
     const id = Number(this.router.snapshot.paramMap.get('id'));
-    this.villainService.getVillain(id).subscribe(villain => this.villain = villain);
+    this.villainService
+      .getVillain(id)
+      .subscribe((villain) => (this.villain = villain));
   }
 
-  goBack():void {
+  goBack(): void {
     this.location.back();
   }
 
   save(): void {
-    if(this.villain){
-      this.villainService.updateVillain(this.villain).subscribe(() => this.goBack())
+    if (this.villain) {
+      this.villainService
+        .updateVillain(this.villain)
+        .subscribe(() => this.goBack());
     }
   }
 }
